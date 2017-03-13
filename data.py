9e9ac800a4ff386e451e071c9a3ad2e4c8734a66
Samarth Sinha <samarth.infofull@gmail.com>
2017-03-13 02:59:42 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+
+
+class Data(object):
+	def __init__(self, relPath, fileName):
+		self.fileName = fileName
+		self.relPath = relPath
+		self.findLongestSentence()
+
+
+	def findLongestSentence(self):
+		self.maxLength = 0
+		os.chdir(self.relPath)
+		with open(self.fileName) as file:
+			for count, line in enumerate(file):
+				match = re.match("(.*)\|([0-9]*$)", line)
+				line = match.group(1)
+				line = line.split()
+				self.maxLen = max(self.maxLen, len(line))
+
+
+	def __str__(self):
+		return str(self.maxLength)
+
+
+
+Data("imdb_data/", "dictionary.txt")
