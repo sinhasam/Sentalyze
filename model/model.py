a1e3b32000d1cf91ccc2655eb584ce33eb36512f
Samarth Sinha <samarth.infofull@gmail.com>
2017-03-27 02:37:13 -0400

    started movement from tf to pytorch

--- model/model.py
+++ model/model.py
@@ -1,6 +1,8 @@
-import tensorflow as tf 
-import numpy as np
-from tensorflow.contrib import rnn
+import numpy as np 
+import torch 
+import torch.nn.functional as F
+from torch.autograd import Variable
+
 os.chdir('..')
 from data import Data 
 
@@ -11,17 +13,30 @@ EPOCH = 10000
 NUM_CLASSES = 5
 NUM_HIDDEN = 128 # commonly chosen hyperparam
 DROPOUT_RATE = 0.5
+EMBEDDING_SIZE = 5
 
-data = Data("imdb_data", "dictionary.txt", MODEL_NAME)
 
-sentence = tf.placeholder("float", [None, EMBEDDING_SIZE, data.maxLength])
-label = tf.placeholder("int32", [None, NUM_CLASSES])
 
-weights = {
-	"weight": tf.Variable(tf.random_normal([2 * NUM_HIDDEN, NUM_CLASSES]))
-}
+class Net(nn.Module):
+    def __init__(self, maxLength, batchSize, embedDim, inputSize, weights = None):
+        super(Net, self).__init__()
+        self.input = Variable(torch.tensor())
+        self.batchSize = batchSize
+        self.inputSize = inputSize
+        self.embedDim = embedDim
+        self.maxLength = maxLength
+        
+        self.setBLSTM()
 
-biases = {
-	"biases": tf.Variable(tf.random_normal([NUM_CLASSES]))
-}
+        if not weights:
+           self.setBLSTM()
 
+        else: 
+            weights = weights
+
+
+    def setBLSTM(self):
+        self.lstm = nn.LSTM(input_size=self.inputSize,hidden_size=NUM_HIDDEN,bidirectional=True,
+                        dropout=DROPOUT_RATE, bias=True)
+        
+        
